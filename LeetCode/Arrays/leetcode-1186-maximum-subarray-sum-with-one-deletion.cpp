2073538dcab4bfaa39d89e29137fc858ef31ab76
y2k-shubham <y2k.shubhamgupta@gmail.com>
2020-11-30 02:24:17 +0530

    (incomplete) [dev] (LeetCode/Arrays/leetcode-1186-maximum-subarray-sum-with-one-deletion) Correct soln

--- LeetCode/Arrays/leetcode-1186-maximum-subarray-sum-with-one-deletion.cpp
+++ LeetCode/Arrays/leetcode-1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -1,4 +1,5 @@
 // LeetCode-1186: https://leetcode.com/problems/maximum-subarray-sum-with-one-deletion/
+// magic print stmt commenting
 
 #include <cassert>
 #include <climits>
@@ -47,6 +48,10 @@ int findMaxEle(vector<int>& vec) {
     }
     return maxEle;
 }
+
+////////
+// wrong
+////////
 
 int findMaxSum(vector<int>& arr, bool debug) {
     int maxSum = 0;
@@ -109,7 +114,8 @@ int findMaxSum(vector<int>& arr, bool debug) {
     return maxSum;
 }
 
-int maximumSumDebug(vector<int>& arr, bool debug) {
+// check 4th test case to understand why this is wrong
+int maximumSumWrongDebug(vector<int>& arr, bool debug) {
     if (allNonPositive(arr)) {
         int maxEle = findMaxEle(arr);
         if (debug) {
@@ -121,30 +127,166 @@ int maximumSumDebug(vector<int>& arr, bool debug) {
     }
 }
 
-void testMaximumSumDebug() {
+void testMaximumSumWrongDebug() {
     vector<int> arr;
 
     arr = {1, -2, 0, 3};
-    assert(maximumSumDebug(arr, false) == 4);
+    assert(maximumSumWrongDebug(arr, false) == 4);
 
     arr = {1, -2, -2, 3};
-    assert(maximumSumDebug(arr, false) == 3);
+    assert(maximumSumWrongDebug(arr, false) == 3);
 
     arr = {-1, -1, -1, -1};
-    assert(maximumSumDebug(arr, false) == -1);
+    assert(maximumSumWrongDebug(arr, false) == -1);
 
     arr = {1, -4, -5, -2, 5, 0, -1, 2};
-    assert(maximumSumDebug(arr, false) == 7);
+    assert(maximumSumWrongDebug(arr, false) == 7);
 
     arr = {11, -10, -11, 8, 7, -6, 9, 4, 11, 6, 5, 0};
-    assert(maximumSumDebug(arr, true) == 50);
+    assert(maximumSumWrongDebug(arr, true) == 50);
 }
 
+////////
+// correct
+////////
+
+vector<int> createMaxEndingHereLeft(vector<int>& vec) {
+    int len = vec.size();
+    vector<int> mehLeft(len, 0);
+
+    int maxSoFar = INT_MIN;
+
+    int maxEndingHere = 0;
+    for (int i = 0; i < len; i++) {
+        int crrEle = vec[i];
+        // printf("\n1. maxEndingHere = %d\n", maxEndingHere);
+        int maxEndingHere = maxEndingHere + crrEle;
+        // printf("vec[i] = %d\n", vec[i]);
+        // printf("2. maxEndingHere = %d\n", maxEndingHere);
+        if (maxEndingHere < crrEle) {
+            maxEndingHere = crrEle;
+        }
+        // printf("3. maxEndingHere = %d\n", maxEndingHere);
+
+        if (maxSoFar < maxEndingHere) {
+            maxSoFar = maxEndingHere;
+        }
+
+        // printf("4. maxEndingHere = %d\n\n", maxEndingHere);
+        mehLeft[i] = maxEndingHere;
+    }
+
+    return mehLeft;
+}
+
+vector<int> createMaxEndingHereRight(vector<int>& vec) {
+    int len = vec.size();
+    vector<int> mehRight(len, 0);
+
+    int maxSoFar = INT_MIN;
+
+    int maxEndingHere = 0;
+    for (int i = len - 1; i >= 0; i--) {
+        int crrEle = vec[i];
+
+        // comment out this statement and then see the magic
+        printf("");
+        // printf("\n1. maxEndingHere = %d\n", maxEndingHere);
+        int maxEndingHere = maxEndingHere + crrEle;
+        if (maxEndingHere < crrEle) {
+            maxEndingHere = crrEle;
+        }
+
+        if (maxSoFar < maxEndingHere) {
+            maxSoFar = maxEndingHere;
+        }
+
+        mehRight[i] = maxEndingHere;
+    }
+
+    return mehRight;
+}
+
+int findMaxSumWithOneDel(vector<int>& vec, vector<int>& mehLeft, vector<int>& mehRight, bool debug) {
+    int len = vec.size();
+    int maxSum = INT_MIN;
+
+    for (int i = 0; i < len; i++) {
+        int maxSumOneDelIncludingCrrEle = 0;
+
+        if (debug) {
+            printf("\n\nat vec[%d] = %d\n", i, vec[i]);
+        }
+
+        if (i == 0) {
+            maxSumOneDelIncludingCrrEle = max(vec[1], 0);
+        } else if (i == len - 1 && len >= 2) {
+            maxSumOneDelIncludingCrrEle = max(vec[len - 2], 0);
+        } else {
+            maxSumOneDelIncludingCrrEle = max(vec[i - 1], 0) + max(vec[i + 1], 0);
+        }
+
+        if (debug) {
+            printf("sumIncCrr = %d + %d = %d\n", maxSumOneDelIncludingCrrEle, vec[i], (maxSumOneDelIncludingCrrEle + vec[i]));
+        }
+        maxSumOneDelIncludingCrrEle = maxSumOneDelIncludingCrrEle + max(vec[i], 0);
+
+        maxSum = max(maxSum, maxSumOneDelIncludingCrrEle);
+    }
+
+    return maxSum;
+}
+
+int maximumSumCorrectDebug(vector<int>& vec, bool debug) {
+    int maxSum = INT_MIN;
+
+    if (allNonPositive(vec)) {
+        maxSum = findMaxEle(vec);
+    } else {
+        vector<int> mehLeft = createMaxEndingHereLeft(vec);
+        vector<int> mehRight = createMaxEndingHereRight(vec);
+        if (debug) {
+            cout << "mehLeft" << endl;
+            showVec(mehLeft, INT_MAX);
+            cout << "mehRight" << endl;
+            showVec(mehRight, INT_MAX);
+        }
+
+        maxSum = findMaxSumWithOneDel(vec, mehLeft, mehRight, debug);
+    }
+
+    return maxSum;
+}
+
+void testMaximumSumCorrectDebug() {
+    vector<int> arr;
+
+    // arr = {1, -2, 0, 3};
+    // assert(maximumSumCorrectDebug(arr, true) == 4);
+
+    // arr = {1, -2, -2, 3};
+    // assert(maximumSumCorrectDebug(arr, false) == 3);
+
+    // arr = {-1, -1, -1, -1};
+    // assert(maximumSumCorrectDebug(arr, false) == -1);
+
+    // arr = {1, -4, -5, -2, 5, 0, -1, 2};
+    // assert(maximumSumCorrectDebug(arr, false) == 7);
+
+    arr = {11, -10, -11, 8, 7, -6, 9, 4, 11, 6, 5, 0};
+    assert(maximumSumCorrectDebug(arr, true) == 50);
+}
+
+////////
+// main
+////////
+
 int maximumSum(vector<int>& arr) {
-    return maximumSumDebug(arr, false);
+    // return maximumSumWrongDebug(arr, false);
+    return maximumSumCorrectDebug(arr, false);
 }
 
 int main() {
-    testMaximumSumDebug();
+    testMaximumSumCorrectDebug();
     return 0;
 }
