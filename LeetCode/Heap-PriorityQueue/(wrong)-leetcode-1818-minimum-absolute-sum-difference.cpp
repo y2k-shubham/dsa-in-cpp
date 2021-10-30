2352b0d405f221c30299631b425a0b6577b213cf
y2k-shubham <y2k.shubhamgupta@gmail.com>
2021-10-30 23:13:38 +0530

    (wrong) [dev] (LeetCode/Heap-PriorityQueue) Rename, add tests for 'leetcode-1818-minimum-absolute-sum-difference'

---
+++ LeetCode/Heap-PriorityQueue/(wrong)-leetcode-1818-minimum-absolute-sum-difference.cpp
@@ -0,0 +1,276 @@
+// LeetCode-1818: https://leetcode.com/problems/minimum-absolute-sum-difference/
+/**
+ * wrong solution
+ * despite passing many test cases, it fails for a vary big test case
+ * nums1: [38,48,73,55,25,47,45,62,15,34,51,20,76,78,38,91,69,69,73,38,74,75,86,63,73,12,100,59,29,28,94,43,100,2,53,31,73,82,70,94,2,38,50,67,8,40,88,87,62,90,86,33,86,26,84,52,63,80,56,56,56,47,12,50,12,59,52,7,40,16,53,61,76,22,87,75,14,63,96,56,65,16,70,83,51,44,13,14,80,28,82,2,5,57,77,64,58,85,33,24]
+ * nums2: [90,62,8,56,33,22,9,58,29,88,10,66,48,79,44,50,71,2,3,100,88,16,24,28,50,41,65,59,83,79,80,91,1,62,13,37,86,53,43,49,17,82,27,17,10,89,40,82,41,2,48,98,16,43,62,33,72,35,10,24,80,29,49,5,14,38,30,48,93,86,62,23,17,39,40,96,10,75,6,38,1,5,54,91,29,36,62,73,51,92,89,88,74,91,87,34,49,56,33,67]
+ * expectedOutput: 3285
+ * computedOutput: 3294
+ */
+
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#define MOD 1000000007
+
+class Solution {
+   private:
+    bool debug = false;
+
+    unordered_set<int> createUnorderedSet(vector<int>& vec) {
+        unordered_set<int> mSet(vec.begin(), vec.end());
+        return mSet;
+    }
+
+    set<int> createOrderedSet(vector<int>& vec) {
+        set<int> mSet(vec.begin(), vec.end());
+        return mSet;
+    }
+
+    pair<int, priority_queue<pair<int, int>>> createDiffHeap(vector<int>& vec1, vector<int>& vec2) {
+        int diffSum = 0;
+
+        priority_queue<pair<int, int>> heap;
+        for (int i = 0; i < vec1.size(); i++) {
+            int diff = abs(vec1[i] - vec2[i]);
+            if (diff == 0) {
+                continue;
+            }
+
+            diffSum += diff;
+            diffSum %= MOD;
+
+            heap.push({diff, i});
+        }
+
+        return {diffSum, heap};
+    }
+
+    int findMaxGain(
+        vector<int>& nums1,
+        vector<int>& nums2,
+        unordered_set<int>& mUnorderedSet,
+        set<int>& mOrderedSet,
+        priority_queue<pair<int, int>>& diffHeap) {
+        int len = nums1.size();
+        if (len <= 0) {
+            return 0;
+        }
+
+        int dipMax = INT_MIN;
+        while (!diffHeap.empty() && ((dipMax == INT_MIN) || (diffHeap.top().first > dipMax))) {
+            int diffOrg = diffHeap.top().first;
+            int idx = diffHeap.top().second;
+            diffHeap.pop();
+
+            int val1 = nums1[idx];
+            int val2 = nums2[idx];
+
+            // check if diff can be made zero (val2 should exist in nums1)
+            if (mUnorderedSet.find(val2) != mUnorderedSet.end()) {
+                dipMax = diffOrg;
+                break;
+            }
+
+            // check how much can be minimize the diff
+            // find the element(s) closest to val2 that are present in nums1
+            set<int>::iterator it = mOrderedSet.lower_bound(val2);
+            int gain = INT_MIN;
+
+            // find element smaller than val2
+            it--;
+            int diffLo = abs(val2 - *it);
+            if (diffLo < diffOrg) {
+                gain = max(gain, diffOrg - diffLo);
+            }
+
+            // find element greater than val2
+            it++;
+            if (it != mOrderedSet.end()) {
+                int diffHi = abs(val2 - *it);
+                if (diffHi < diffOrg) {
+                    gain = max(gain, diffOrg - diffHi);
+                }
+            }
+
+            if ((gain != INT_MIN) && (gain > dipMax)) {
+                dipMax = gain;
+            }
+        }
+
+        return max(dipMax, 0);
+    }
+
+   public:
+    friend class SolutionTest;
+    int minAbsoluteSumDiff(vector<int>& nums1, vector<int>& nums2) {
+        int len = nums1.size();
+        if (len == 0) {
+            return 0;
+        }
+
+        pair<int, priority_queue<pair<int, int>>> diffsResponse = createDiffHeap(nums1, nums2);
+        int orgDiffSum = diffsResponse.first;
+        priority_queue<pair<int, int>> diffHeap = diffsResponse.second;
+
+        if ((len == 1) || (orgDiffSum == 0)) {
+            return orgDiffSum;
+        }
+
+        unordered_set<int> mUnorderedSet = createUnorderedSet(nums1);
+        set<int> mOrderedSet = createOrderedSet(nums1);
+
+        int dipMax = findMaxGain(nums1, nums2, mUnorderedSet, mOrderedSet, diffHeap);
+        int reducedDiffSum = (orgDiffSum - dipMax) % MOD;
+
+        return reducedDiffSum;
+    }
+};
+
+class SolutionTest {
+   public:
+    void testFindMaxGain() {
+        Solution soln = Solution();
+        vector<int> nums1, nums2;
+        unordered_set<int> mUnorderedSet;
+        set<int> mOrderedSet;
+        priority_queue<pair<int, int>> diffHeap;
+        int outExpected, outComputed;
+
+        nums1 = {};
+        nums2 = {};
+        mUnorderedSet = soln.createUnorderedSet(nums1);
+        mOrderedSet = soln.createOrderedSet(nums1);
+        diffHeap = soln.createDiffHeap(nums1, nums2).second;
+        outExpected = 0;
+        outComputed = soln.findMaxGain(nums1, nums2, mUnorderedSet, mOrderedSet, diffHeap);
+        assert(outExpected == outComputed);
+
+        nums1 = {3};
+        nums2 = {4};
+        mUnorderedSet = soln.createUnorderedSet(nums1);
+        mOrderedSet = soln.createOrderedSet(nums1);
+        diffHeap = soln.createDiffHeap(nums1, nums2).second;
+        outExpected = 0;
+        outComputed = soln.findMaxGain(nums1, nums2, mUnorderedSet, mOrderedSet, diffHeap);
+        assert(outExpected == outComputed);
+
+        nums1 = {3, 5};
+        nums2 = {4, 9};
+        mUnorderedSet = soln.createUnorderedSet(nums1);
+        mOrderedSet = soln.createOrderedSet(nums1);
+        diffHeap = soln.createDiffHeap(nums1, nums2).second;
+        outExpected = 0;
+        outComputed = soln.findMaxGain(nums1, nums2, mUnorderedSet, mOrderedSet, diffHeap);
+        assert(outExpected == outComputed);
+
+        nums1 = {6, 5};
+        nums2 = {4, 9};
+        mUnorderedSet = soln.createUnorderedSet(nums1);
+        mOrderedSet = soln.createOrderedSet(nums1);
+        diffHeap = soln.createDiffHeap(nums1, nums2).second;
+        outExpected = 1;
+        outComputed = soln.findMaxGain(nums1, nums2, mUnorderedSet, mOrderedSet, diffHeap);
+        assert(outExpected == outComputed);
+
+        nums1 = {6, 5};
+        nums2 = {3, 11};
+        mUnorderedSet = soln.createUnorderedSet(nums1);
+        mOrderedSet = soln.createOrderedSet(nums1);
+        diffHeap = soln.createDiffHeap(nums1, nums2).second;
+        outExpected = 1;
+        outComputed = soln.findMaxGain(nums1, nums2, mUnorderedSet, mOrderedSet, diffHeap);
+        assert(outExpected == outComputed);
+
+        nums1 = {2, 0};
+        nums2 = {2, 3};
+        mUnorderedSet = soln.createUnorderedSet(nums1);
+        mOrderedSet = soln.createOrderedSet(nums1);
+        diffHeap = soln.createDiffHeap(nums1, nums2).second;
+        outExpected = 2;
+        outComputed = soln.findMaxGain(nums1, nums2, mUnorderedSet, mOrderedSet, diffHeap);
+        assert(outExpected == outComputed);
+
+        nums1 = {2, 4};
+        nums2 = {2, 2};
+        mUnorderedSet = soln.createUnorderedSet(nums1);
+        mOrderedSet = soln.createOrderedSet(nums1);
+        diffHeap = soln.createDiffHeap(nums1, nums2).second;
+        outExpected = 2;
+        outComputed = soln.findMaxGain(nums1, nums2, mUnorderedSet, mOrderedSet, diffHeap);
+        assert(outExpected == outComputed);
+
+        nums1 = {1, 7, 5};
+        nums2 = {2, 3, 5};
+        mUnorderedSet = soln.createUnorderedSet(nums1);
+        mOrderedSet = soln.createOrderedSet(nums1);
+        diffHeap = soln.createDiffHeap(nums1, nums2).second;
+        outExpected = 2;
+        outComputed = soln.findMaxGain(nums1, nums2, mUnorderedSet, mOrderedSet, diffHeap);
+        assert(outExpected == outComputed);
+
+        nums1 = {1, 10, 4, 4, 2, 7};
+        nums2 = {9, 3, 5, 1, 7, 4};
+        mUnorderedSet = soln.createUnorderedSet(nums1);
+        mOrderedSet = soln.createOrderedSet(nums1);
+        diffHeap = soln.createDiffHeap(nums1, nums2).second;
+        outExpected = 7;
+        outComputed = soln.findMaxGain(nums1, nums2, mUnorderedSet, mOrderedSet, diffHeap);
+        assert(outExpected == outComputed);
+    }
+
+    void testMinAbsoluteSumDiff() {
+        Solution soln;
+        vector<int> nums1, nums2;
+        int outExpected, outComputed;
+
+        nums1 = {};
+        nums2 = {};
+        outExpected = 0;
+        outComputed = soln.minAbsoluteSumDiff(nums1, nums2);
+        assert(outExpected == outComputed);
+
+        nums1 = {2, 3};
+        nums2 = {2, 3};
+        outExpected = 0;
+        outComputed = soln.minAbsoluteSumDiff(nums1, nums2);
+        assert(outExpected == outComputed);
+
+        nums1 = {1, 7, 5};
+        nums2 = {2, 3, 5};
+        outExpected = 3;
+        outComputed = soln.minAbsoluteSumDiff(nums1, nums2);
+        assert(outExpected == outComputed);
+
+        nums1 = {2, 4, 6, 8, 10};
+        nums2 = {2, 4, 6, 8, 10};
+        outExpected = 0;
+        outComputed = soln.minAbsoluteSumDiff(nums1, nums2);
+        assert(outExpected == outComputed);
+
+        nums1 = {1, 10, 4, 4, 2, 7};
+        nums2 = {9, 3, 5, 1, 7, 4};
+        outExpected = 20;
+        outComputed = soln.minAbsoluteSumDiff(nums1, nums2);
+        assert(outExpected == outComputed);
+    }
+};
+
+int main() {
+    SolutionTest solnTest = SolutionTest();
+
+    solnTest.testFindMaxGain();
+    solnTest.testMinAbsoluteSumDiff();
+
+    return 0;
+}
