2b1850298c6b317f6db9c20e89cdb54a1fc21a89
y2k-shubham <y2k.shubhamgupta@gmail.com>
2021-11-13 13:46:57 +0530

    (complete) [dev][doc] (LeetCode/SlidingWindow) Add correct soln for 'leetcode-424-longest-repeating-character-replacement'

--- LeetCode/SlidingWindow/leetcode-424-longest-repeating-character-replacement.cpp
+++ LeetCode/SlidingWindow/leetcode-424-longest-repeating-character-replacement.cpp
@@ -1,19 +1,24 @@
 // LeetCode-424: https://leetcode.com/problems/longest-repeating-character-replacement/
-// wrong soln; anyways not easy to think and also quite a feq edge cases
-// (approach was right, just implementation was wrong)
+// NickWhite: https://www.youtube.com/watch?v=00FmUN1pkGE
 
+#include <algorithm>
 #include <cassert>
 #include <cmath>
 #include <cstdio>
 #include <iostream>
 #include <map>
+#include <numeric>
 #include <set>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
+#define NUM_ALPHAS 26
+
 class Solution {
    private:
+    bool debug;
     int findLongestSameCharSubstrLen(string str) {
         int len = str.size();
 
@@ -36,7 +41,21 @@ class Solution {
         return longestSubstrLen;
     }
 
+    int findMaxVal(vector<int>& vec) {
+        return *max_element(vec.begin(), vec.end());
+    }
+
+    void showFreqVec(vector<int>& vec) {
+        for (int i = 0; i < vec.size(); i++) {
+            if (vec[i] > 0) {
+                printf("'%c' -> %d\n", ((char)('A' + i)), vec[i]);
+            }
+        }
+    }
+
    public:
+    friend class SolutionTest;
+
     int characterReplacement(string s, int k) {
         int len = s.size();
         if (len <= 1) {
@@ -46,77 +65,56 @@ class Solution {
         if (k == 0) {
             return findLongestSameCharSubstrLen(s);
         }
+        if (k >= len) {
+            return len;
+        }
 
-        int maxSubstringLen = -1;
-        map<char, int> freqMap;
-        map<int, set<char> > freqCharsMap;
-
+        vector<int> freqVec(NUM_ALPHAS, 0);
+        int maxWinLen = -1;
         int iLo = -1;
         int iHi = -1;
         while ((iLo <= iHi) && (iHi < len)) {
-            // expand window till there are exactly 'k' other chars in window
-            // apart from the highest frequency char
-            while (iHi < len) {
-                iHi++;
-                if (iHi == 0) {
-                    iLo = 0;
-                }
-                if (iHi >= len) {
-                    break;
-                }
-
-                char chIn = s.at(iHi);
-                int prevFreq = freqMap[chIn];
-                int newFreq = prevFreq + 1;
-                if (prevFreq > 0) {
-                    freqCharsMap[prevFreq].erase(chIn);
-                }
-                freqCharsMap[newFreq].insert(chIn);
-                freqMap[chIn]++;
-
-                int maxFreq = freqMap[*freqCharsMap.rbegin()->second.begin()];
-                int winSize = iHi - iLo + 1;
-                int numNonMaxFreqChars = winSize - maxFreq;
-                if (numNonMaxFreqChars <= k) {
-                    // 'k' or lesser undesired chars in window that can be defaced (replaced)
-                    maxSubstringLen = max(maxSubstringLen, winSize);
-                } else {
-                    // more than 'k' undesired chars in window
-                    break;
-                }
+            iHi++;
+            if (iHi == 0) {
+                iLo = 0;
+            }
+            if (iHi >= len) {
+                break;
             }
 
-            // shrink window till there are more than 'k' undesired chars in window
-            while (iLo < iHi) {
+            char chIn = s.at(iHi);
+            freqVec[chIn - 'A']++;
+
+            int maxFreq = findMaxVal(freqVec);
+            if (debug) {
+                printf("\n");
+                printf("s[iLo=%d .. iHi=%d] = %c .. %c, included s[iHi=%d] = %c\n", iLo, iHi, s.at(iLo), s.at(iHi), iHi, s.at(iHi));
+                showFreqVec(freqVec);
+                printf("maxFreq = %d\n", maxFreq);
+            }
+
+            // keep shrinking the window till there are more than 'k' chars to replace
+            while ((iLo < iHi) && ((iHi - iLo + 1 - maxFreq) > k)) {
                 char chOut = s.at(iLo);
-                int prevFreq = freqMap[chOut];
-                int newFreq = prevFreq - 1;
-                if (newFreq > 0) {
-                    freqCharsMap[newFreq].insert(chOut);
-                }
-                freqCharsMap[prevFreq].erase(chOut);
-                freqMap[chOut]--;
-
                 iLo++;
-                if (iLo >= iHi) {
-                    break;
-                }
 
-                int maxFreq = freqMap[*freqCharsMap.rbegin()->second.begin()];
-                int winSize = iHi - iLo + 1;
-                int numNonMaxFreqChars = winSize - maxFreq;
-                if (numNonMaxFreqChars <= k) {
-                    // 'k' or lesser undesired chars in window that can be defaced (replaced)
-                    maxSubstringLen = max(maxSubstringLen, winSize);
-                    break;
-                } else {
-                    // more than 'k' undesired chars in window
-                    continue;
+                freqVec[chOut - 'A']--;
+
+                maxFreq = findMaxVal(freqVec);
+
+                if (debug) {
+                    printf("\n");
+                    printf("s[iLo=%d .. iHi=%d] = %c .. %c, excluded s[iLo-1=%d] = %c\n", iLo, iHi, s.at(iLo), s.at(iHi), iLo - 1, s.at(iLo - 1));
+                    showFreqVec(freqVec);
+                    printf("maxFreq = %d\n", maxFreq);
                 }
             }
+
+            int crrWinLen = iHi - iLo + 1;
+            maxWinLen = max(maxWinLen, crrWinLen);
         }
 
-        return maxSubstringLen;
+        return maxWinLen;
     }
 };
 
@@ -158,11 +156,14 @@ class SolutionTest {
         int k;
         int outExpected, outComputed;
 
+        // soln.debug = true;
         str = "ABAB";
         k = 2;
         outExpected = 4;
         outComputed = soln.characterReplacement(str, k);
+        // printf("outComputed = %d\n", outComputed);
         assert(outExpected == outComputed);
+        // soln.debug = false;
 
         str = "AABABBA";
         k = 1;
@@ -210,8 +211,8 @@ class SolutionTest {
         k = 2;
         outExpected = 6;
         outComputed = soln.characterReplacement(str, k);
-        pair<int, int> winIdx = findMaxSingleCharFreqWindow(str, 6);
-        printf("%d, %d\n", winIdx.first, winIdx.second);
+        // pair<int, int> winIdx = findMaxSingleCharFreqWindow(str, 6);
+        // printf("%d, %d\n", winIdx.first, winIdx.second);
         assert(outExpected == outComputed);
     }
 };
