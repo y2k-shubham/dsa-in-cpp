8dd69bf2b3940650a820b490dfcd7af3f1b62648
y2k-shubham <y2k.shubhamgupta@gmail.com>
2021-12-19 12:06:46 +0530

    (complete) [dev] (LeetCode/SlidingWindow/leetcode-713-subarray-product-less-than-k) Add correct soln

--- LeetCode/SlidingWindow/leetcode-713-subarray-product-less-than-k.cpp
+++ LeetCode/SlidingWindow/leetcode-713-subarray-product-less-than-k.cpp
@@ -1,9 +1,9 @@
 // LeetCode-713: https://leetcode.com/problems/subarray-product-less-than-k/
 
+#include <cassert>
 #include <cstdio>
 #include <iostream>
 #include <vector>
-#include <cassert>
 
 using namespace std;
 
@@ -14,46 +14,31 @@ class Solution {
     }
 
    public:
+    // soln takes from LeetCode itself (Solution section)
+    // (appears absurdly simple)
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         int len = nums.size();
-        if (len == 0) {
+        if (len <= 0) {
             return 0;
         }
-        if (len == 1) {
-            return (nums[0] < k) ? 1 : 0;
-        }
-        if (k == 0) {
+        // dont understand this expression
+        if (k <= 1) {
             return 0;
         }
 
         int totalNumSubarrs = 0;
 
-        int iLo = 0;
-        int iHi = 0;
-        int crrWinProd = 1;
+        int crrSubarrProd = 1;
+        int iLo = 0, iHi = 0;
         while (iHi < len) {
-            // keep expanding window until product is less than k
-            crrWinProd *= nums[iHi];
+            crrSubarrProd *= nums[iHi];
 
-            if (crrWinProd >= k) {
-                int prevWinSize = iHi - 1 - iLo + 1;
-                int prevWinNumSubarrs = sigmaN(prevWinSize);
-                totalNumSubarrs += prevWinNumSubarrs;
-                printf(
-                    "nums[%d]=%d .. nums[%d]=%d; prod=%d, prevWinSize=%d, prevNumSubarrs=%d, totalNumSubarrs=%d\n",
-                    iLo, nums[iLo],
-                    iHi, nums[iHi],
-                    crrWinProd,
-                    prevWinSize,
-                    prevWinNumSubarrs,
-                    totalNumSubarrs);
-
-                // keep shrinking window until product is greater than or equal to k
-                while ((iLo <= iHi) && (crrWinProd >= k)) {
-                    crrWinProd /= nums[iLo];
-                    iLo++;
-                }
+            while (crrSubarrProd >= k) {
+                crrSubarrProd /= nums[iLo++];
             }
+
+            int numSubarrsEndingAtIHi = (iLo <= iHi) ? (iHi - iLo + 1) : 0;
+            totalNumSubarrs += numSubarrsEndingAtIHi;
 
             iHi++;
         }
