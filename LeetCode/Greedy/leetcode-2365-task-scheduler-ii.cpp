1e341015c81e892b065b6516e8c66afa468f30e9
y2k-shubham <y2k.shubhamgupta@gmail.com>
2025-01-23 23:58:28 +0530

    checkout commit

--- LeetCode/Greedy/leetcode-2365-task-scheduler-ii.cpp
+++ LeetCode/Greedy/leetcode-2365-task-scheduler-ii.cpp
@@ -12,20 +12,20 @@ using namespace std;
 
 class Solution {
 private:
-    unordered_map<int, int> createFreqMap(vector <int>& vec) {
-        unordered_map<int, int> freqMap;
+    unordered_map <int, int> createFreqMap(vector <int>& vec) {
+        unordered_map <int, int> freqMap;
 
-        for (int val: vec) {
+        for (int val : vec) {
             freqMap[val]++;
         }
 
         return freqMap;
     }
 
-    vector <pair <int, int> > createFreqValVec(unordered_map<int, int>& freqMap) {
+    vector <pair <int, int>> createFreqValVec(unordered_map <int, int>& freqMap) {
         int numUniqueItems = freqMap.size();
 
-        vector <pair <int, int> > freqValVec(numUniqueItems);
+        vector <pair <int, int>> freqValVec(numUniqueItems);
         int i = 0;
         for (auto it = freqMap.begin(); it != freqMap.end(); it++) {
             freqValVec[i++] = {it->second, it->first};
@@ -34,13 +34,31 @@ private:
         return freqValVec;
     }
 
+    long long calculateFreqSum(vector <pair <int, int>>& freqValVec) {
+        long long freqSum = 0;
+        for (pair <int, int> freqVal : freqValVec) {
+            freqSum += freqVal.first;
+        }
 
+        return freqSum;
+    }
 
 public:
-    long long taskSchedulerII(vector<int>& tasks, int space) {
-        unordered_map<int, int> freqMap = createFreqMap(tasks);
+    long long taskSchedulerII(vector <int>& tasks, int space) {
+        unordered_map <int, int> freqMap = createFreqMap(tasks);
 
-        vector <pair <int, int> > freqValVec = createFreqValVec(freqMap);
-        sort(freqValVec.begin(), freqValVec.end(), greater<>());
+        vector <pair <int, int>> freqValVec = createFreqValVec(freqMap);
+        sort(freqValVec.begin(), freqValVec.end(), greater <>());
+
+        long long maxFreq = freqValVec[0].first;
+
+        long long freqSum = calculateFreqSum(freqValVec);
+        long long freqSumWithoutMaxFreq = freqSum - maxFreq;
+
+        if (freqSumWithoutMaxFreq >= (maxFreq - 1)) {
+            return freqSum;
+        } else {
+            return (freqSumWithoutMaxFreq * 2) + 1;
+        }
     }
 };
