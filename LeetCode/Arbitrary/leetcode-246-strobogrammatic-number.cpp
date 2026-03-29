3c7a0bb9fb09f3060aa118c93db103915cabd661
y2k-shubham <y2k.shubhamgupta@gmail.com>
2026-03-29 16:47:26 +0530

    [dev] (LeetCode/Arbitrary) Create 'leetcode-246-strobogrammatic-number'
    URL: https://leetcode.com/problems/strobogrammatic-number

---
+++ LeetCode/Arbitrary/leetcode-246-strobogrammatic-number.cpp
@@ -0,0 +1,65 @@
+// LeetCode-246: https://leetcode.com/problems/strobogrammatic-number
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    vector<bool> symmetricDigits;
+    vector<char> reflectionDigits;
+
+private:
+    void initSymmetricDigits() {
+        symmetricDigits = vector<bool>(128, false);
+        symmetricDigits[0] = true;
+        symmetricDigits[1] = true;
+        symmetricDigits[8] = true;
+        symmetricDigits['0'] = true;
+        symmetricDigits['1'] = true;
+        symmetricDigits['8'] = true;
+    }
+
+    void initReflectionDigits() {
+        reflectionDigits = vector<char>(128, -1);
+        reflectionDigits[6] = 9;
+        reflectionDigits[9] = 6;
+        reflectionDigits['6'] = '9';
+        reflectionDigits['9'] = '6';
+    }
+
+    void init() {
+        initSymmetricDigits();
+        initReflectionDigits();
+    }
+
+    bool areStrobogrammaticDigits(char digit1, char digit2) {
+        if (digit1 == digit2) {
+            return symmetricDigits[digit1];
+        } else {
+            return (reflectionDigits[digit1] == digit2);
+        }
+    }
+
+    bool areStrobogrammaticDigits(int digit1, int digit2) {
+        return areStrobogrammaticDigits((char)digit1, (char)digit2);
+    }
+
+public:
+    bool isStrobogrammatic(string num) {
+        init();
+
+        int len = num.size();
+
+        for (int i = 0; i <= (len / 2); i++) {
+            char digit1 = num[i];
+            char digit2 = num[len - 1 - i];
+
+            if (!areStrobogrammaticDigits(digit1, digit2)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
